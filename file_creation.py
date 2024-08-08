82c510d647d22306137c063f587b49bbdd2501d0
vondrakmar <vondrakmar@gmail.com>
2024-08-08 16:53:44 +0200

    fhi_aims preparation hopefully done

--- file_creation.py
+++ file_creation.py
@@ -1,4 +1,4 @@
-def prep_aims_ase_file(base_file="aims_ase_template.py",final_name="runAims.py",aims_command="raven",aims_species="raven/species"):
+def prep_aims_ase_file(base_file="aims_ase_template.py",final_name="runAims.py",aims_command="raven",aims_species="raven/species/"):
     with open(base_file, 'r') as base_file:
         base_lines = base_file.read()
     with open(f'{final_name}', 'w') as final_file:
@@ -6,4 +6,20 @@ def prep_aims_ase_file(base_file="aims_ase_template.py",final_name="runAims.py",
         final_file.write(f'aims_species="{aims_species}"\n')
         final_file.write("\n"+base_lines)
 
-prep_aims_ase_file()
+# prep_aims_ase_file()
+def prep_submit_header(submitted_nodes,NTASK_PER_NODE,PRESETS_FOR_HEADER,wall_time="1:00:00"):
+    header_file = open("header_file.temp","w")
+    header_file.write("#!/bin/bash -l\n")
+    header_file.write("#SBATCH -o ./tjob.out.%j\n") # stardart output file
+    header_file.write("#SBATCH -e ./tjob.err.%j\n") # error output file
+    header_file.write("#SBATCH -D ./\n") # working directory 
+    # header_file.write(f"#SBATCH -J {job_name}\n") # name of the file
+    header_file.write("\n")
+    header_file.write(f"#SBATCH --nodes={submitted_nodes}\n")
+    header_file.write(f"#SBATCH --ntasks-per-node={NTASK_PER_NODE}\n")
+    header_file.write(f"#SBATCH --ntasks-per-node=1\n")
+    header_file.write(f"#SBATCH --time={wall_time}\n")
+    header_file.write("\n")
+    header_file.write(f"{PRESETS_FOR_HEADER}")
+    #header_file.write("#SBATCH --cpus-per-task=1\n")
+    header_file.close()
--- hpc_workflow.py
+++ hpc_workflow.py
@@ -1,5 +1,6 @@
 import os
 import my_presets
+import file_creation
 '''
 the script prep_aims.sh is used as bash prep_aims.sh aimsRun.py temp at_the_same_time in_one_file
 herein I am calculating how many of at_the_same_time and in_one_file should be used based on sbatch setting
@@ -16,12 +17,15 @@ parser.add_argument("--prep_submit",help="If true, script will create an header 
 parser.add_argument("--job_name","-J",help="Name that will be put into the header of submit file",type=str,default="aims_job")
 parser.add_argument("--strucs",help="File from which DFT folders should be prepared",type=str)
 parser.add_argument("--strucs_ext",help="Extension of the strucs file",type=str,default="extxyz")
+parser.add_argument("--method",help="Name of code you want to use",type=str,choices=["aims"])
+parser.add_argument("--aims_basis",choices=["light","intermediate","tight"])
+
 args = parser.parse_args()
 
 ###########
 per_file = 64
 extension = ".xyz"
-aims_run_file = "aimsRun.py"
+aims_run_file = "aims_run.py"
 ########3
 hpc_setting = args.hpc 
 # Raven setting
@@ -30,6 +34,9 @@ if hpc_setting == "raven":
     MEMORY_PER_NODE_GB = 240 
     MEMORY_PER_NODE_MB = 240000 
     PRESETS_FOR_HEADER = my_presets.aims_for_raven
+    AIMS_EXEC = my_presets.aims_exec_raven
+    AIMS_PATH = my_presets.aims_path_raven
+    AIMS_SPECIEC = my_presets.aims_species_raven
 # Viper setting
 elif hpc_setting =="viper":
     CPUS_PER_NODE = 128
@@ -60,22 +67,19 @@ at_the_same_time = int(at_the_same_time)
 print(f"bash prep_aims.sh aimsRun.py temp {at_the_same_time} {per_file}")
 if args.prep_submit:
     job_name=args.job_name
-    header_file = open("header_file.temp","w")
-    header_file.write("#!/bin/bash -l\n")
-    header_file.write("#SBATCH -o ./tjob.out.%j\n") # stardart output file
-    header_file.write("#SBATCH -e ./tjob.err.%j\n") # error output file
-    header_file.write("#SBATCH -D ./\n") # working directory 
-    # header_file.write(f"#SBATCH -J {job_name}\n") # name of the file
-    header_file.write("\n")
-    header_file.write(f"#SBATCH --nodes={submitted_nodes}\n")
-    header_file.write(f"#SBATCH --ntasks-per-node={CPUS_PER_NODE}\n")
-    header_file.write("\n")
-    header_file.write(f"{PRESETS_FOR_HEADER}")
-    #header_file.write("#SBATCH --cpus-per-task=1\n")
-    header_file.close()
+    file_creation.prep_submit_header(submitted_nodes,CPUS_PER_NODE,PRESETS_FOR_HEADER)
+
+if args.method == "aims":
+    file_creation.prep_aims_ase_file(final_name=aims_run_file,
+                                     aims_command=f"{AIMS_PATH}{AIMS_EXEC}",
+                                     aims_species=f"{AIMS_PATH}{AIMS_SPECIEC}{args.aims_basis}")
+
+
 
 prep_folders = True
 if prep_folders:
+    with open("header_file.temp","r") as head_file:
+        head_data= head_file.read()
     import ase.io
     import shutil
     mols = ase.io.read(f"{args.strucs}@:",format=f"{args.strucs_ext}")
@@ -87,13 +91,15 @@ if prep_folders:
             if id_mol != 0:
                 slurm_file.close()
                 paral_file.close()
-            slurm_file = open(f"submit_file{count}.sl","w")
             count+=1
-            paral_file = open(f"paral_file{count}","w")            
+            slurm_file = open(f"submit_file{count}.sl","w")
+            slurm_file.write(head_data)
+            slurm_file.write(f"parallel --delay 0.2 --joblog task.log --progress -j {at_the_same_time} < paral_file{count}")
+            paral_file = open(f"paral_file{count}","w")
         dir_name = f"struc{id_mol:0{counting_digits}}/"
         struc_file_name = f"struc{id_mol:0{counting_digits}}{extension}" 
         ase.io.write(struc_file_name,mol,format=f"{args.strucs_ext}")
         os.mkdir(dir_name)
         shutil.move(struc_file_name,dir_name)
         shutil.copy(aims_run_file,dir_name)
-        paral_file.write(f"cd {dir_name}; python aimsRun.py {struc_file_name    } {node_per_job} {cpu_per_job_to_srun}")   
+        paral_file.write(f"cd {dir_name}; python aimsRun.py {struc_file_name    } {node_per_job} {cpu_per_job_to_srun}\n")
