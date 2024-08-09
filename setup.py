4517c466f61459e609dd11ee2370fe392a151b52
vondrakmar <vondrakmar@gmail.com>
2024-08-09 16:33:40 +0200

    added setup.py and moved to jobcraft folder

---
+++ setup.py
@@ -0,0 +1,10 @@
+import sys
+from setuptools import setup, find_packages, Extension
+
+
+setup(
+    name='jobcraft',
+    version='0.1',
+    packages=['jobcraft'],
+    author='Martin Vondrak',
+)
